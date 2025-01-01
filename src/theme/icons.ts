25c536868a285666fbceb51ca2dd9a844846bcc7
Purrseus <contact@thiendo.dev>
2025-01-01 23:38:28 +0700

    fix: icon paths

--- src/theme/icons.ts
+++ src/theme/icons.ts
@@ -1,9 +1,9 @@
 const icons = Object.freeze({
-  bug: require('./assets/icons/bug.png'),
-  delete: require('./assets/icons/delete.png'),
-  hide: require('./assets/icons/hide.png'),
-  move: require('./assets/icons/move.png'),
-  record: require('./assets/icons/record.png'),
+  bug: require('../assets/icons/bug.png'),
+  delete: require('../assets/icons/delete.png'),
+  hide: require('../assets/icons/hide.png'),
+  move: require('../assets/icons/move.png'),
+  record: require('../assets/icons/record.png'),
 });
 
 export default icons;
